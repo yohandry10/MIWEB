92a82e194b88ce39f868f03678dea7ae8ff05fc2
yohandry10 <yohandrychirinos1@gmail.com>
2024-10-18 02:15:21 -0500

    l

--- miweb/src/index.js
+++ miweb/src/index.js
@@ -6,7 +6,6 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// Importamos la configuración de i18n
 import './i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
